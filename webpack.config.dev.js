9045d51ff77dbb3700af2974a65023c83e65efc5
MaartenGDev <maarten.grootoonk@gmail.com>
2017-03-21 21:22:37 +0100

    Added Webpack setup

---
+++ webpack.config.dev.js
@@ -0,0 +1,35 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
+        path.resolve(__dirname, 'src/index')
+    ],
+    target: 'web',
+    output: {
+        path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'src'),
+        noInfo: false,
+        debug: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    ],
+    module: {
+        loaders: [
+            {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel-loader']},
+            {test: /(\.css)$/, loaders: ['style', 'css']},
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+            {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+        ]
+    }
+};
